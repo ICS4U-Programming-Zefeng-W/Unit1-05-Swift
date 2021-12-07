8ecdb7842936df0fa90ac72e902756b208df0c57
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-07 03:56:08 +0000

    Fixed comments

--- BoardFoot.swift
+++ BoardFoot.swift
@@ -9,17 +9,20 @@
   of 144 inches^3. 
  */
 
+// Calculates the length of a board foot given its width and height
 func calculateBoardFoot(width: Double, height: Double) -> Double {
 	let boardFootVol = Double(144)
 	return boardFootVol / (width * height)
 }
 
 while true {
+	// Ask the user for the width and height of the board foot and reads them
 	print("Please enter the width: ")
 	if let width = Double(readLine()!) {
 		print("Please enter the height: ")
 		if let height = Double(readLine()!) {
 			let length = calculateBoardFoot(width: width, height: height)
+			// Display the length
 			print("Length is \(length) inch(es),height is \(height) inch(es), and width is \(width) inch(es)\n")
 			break
 		} else {
